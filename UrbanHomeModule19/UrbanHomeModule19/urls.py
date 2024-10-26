7b45bd4c88335e63859533da1dd44ad716a2873b
Nastya <nastysha7069@yandex.ru>
2024-10-26 22:45:29 +0300

    M 19 ДЗ 3 "MVT. Вывод объектов в шаблоны".

--- UrbanHomeModule19/UrbanHomeModule19/urls.py
+++ UrbanHomeModule19/UrbanHomeModule19/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task1.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('platform/', platform),
+    path('platform/games/', games),
+    path('platform/cart/', cart),
+    path('platform/menu/', menu),
+    path('django_sign_up/', sign_up_by_html),
+    path('', sign_up_by_django),
 ]
--- UrbanHomeModule19/task1/views.py
+++ UrbanHomeModule19/task1/views.py
@@ -1,3 +1,109 @@
 from django.shortcuts import render
+from .forms import UserRegister
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+users = []
+buyers = Buyer.objects.all()
+for buyer in buyers:
+    users.append(buyer.name)
+
+info = {}
+
+
+def platform(request):
+    title = 'Главная страница'
+    context = {
+        'title': title
+    }
+    return render(request, 'platform.html', context)
+
+
+def games(request):
+    title = 'Игры'
+    button_pay = 'Купить'
+    games = []
+    Games = Game.objects.all()
+    for game in Games:
+        games.append(f'{game.title} | {game.description}. Стоимость: {game.cost}')
+    context = {
+        'title': title,
+        'games': games,
+        'button_pay': button_pay,
+    }
+    return render(request, 'games.html', context)
+
+
+def cart(request):
+    title = 'Корзина'
+    text = 'Вернитесь в магазин и купите что-нибудь'
+    text2 = 'Ну пожалуйста)'
+    context = {
+        'title': title,
+        'text': text,
+        'text2': text2,
+    }
+    return render(request, 'cart.html', context)
+
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
+def sign_up_by_html(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+
+        if password == repeat_password and int(age) >= 18 and not username in users:
+            Buyer.objects.create(name=username, balance=0, age=age)
+            return HttpResponse(f'Приветствуем, {username}!')
+        elif password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        elif int(age) < 18:
+            info['error'] = 'Вы должны быть старше 18'
+        elif username in users:
+            info['error'] = 'Пользователь уже существует'
+        info['username'] = username
+        info['password'] = password
+        info['repeat_password'] = repeat_password
+        info['age'] = age
+        return render(request, 'registration_page.html', info)
+    info['error'] = ''
+    info['username'] = ''
+    info['password'] = ''
+    info['repeat_password'] = ''
+    info['age'] = ''
+    return render(request, 'registration_page.html', info)
+
+
+def sign_up_by_django(request):
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+            if password == repeat_password and int(age) >= 18 and not username in users:
+                Buyer.objects.create(name=username, balance=0, age=age)
+                return HttpResponse(f'Приветствуем, {username}!')
+            elif password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+            elif int(age) < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            elif username in users:
+                info['error'] = 'Пользователь уже существует'
+            info['username'] = username
+            info['password'] = password
+            info['repeat_password'] = repeat_password
+            info['age'] = age
+            info['form'] = form
+            return render(request, 'registration_page.html', info)
+    else:
+        form = UserRegister()
+    return render(request, 'registration_page.html', {'form': form})
+
